22afcffb49d9f6718e7ec6d78449fa129f9ee6b9
Bryan Dao <bryandd1@uci.edu>
2025-03-11 03:25:13 +0000

    commiter

--- src/ladder.cpp
+++ src/ladder.cpp
@@ -1,5 +1,5 @@
 #include "ladder.h"
-
+#define my_assert(e) {cout << #e << ((e) ? " passed": " failed") << endl;}
 
 void error(string word1, string word2, string msg) {
     cout << msg << word1 << word2;
@@ -85,4 +85,14 @@ void print_word_ladder(const vector<string>& ladder) {
 }
 
 
-void verify_word_ladder();
+void verify_word_ladder() {
+    set<string> word_list;
+    load_words(word_list, "words.txt");
+    my_assert(generate_word_ladder("cat", "dog", word_list).size() == 4);
+    my_assert(generate_word_ladder("marty", "curls", word_list).size() == 6);
+    my_assert(generate_word_ladder("code", "data", word_list).size() == 6);
+    my_assert(generate_word_ladder("work", "play", word_list).size() == 6);
+    my_assert(generate_word_ladder("sleep", "awake", word_list).size() == 8);
+    my_assert(generate_word_ladder("car", "cheat", word_list).size() == 4);
+}
+
