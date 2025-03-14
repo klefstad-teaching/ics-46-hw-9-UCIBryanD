11459edcba6552cdf522dad650a19482c89c7ef2
Bryan Dao <bryandd1@uci.edu>
2025-03-14 04:27:09 +0000

    dijstraks crash fix via vector initialization

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -1,8 +1,10 @@
 #include "dijkstras.h"
 
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
+    //cout << "INF Value: " << INF << endl;
     int num_vertices = G.numVertices;
-    vector<int> distances{num_vertices, INF};
+    vector<int> distances;
+    distances.resize(num_vertices, INF);
     vector<bool> visited;
     visited.resize(num_vertices, false);
     distances[source] = 0;
@@ -14,19 +16,26 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     while(!min_heap.empty()) {
         int u = min_heap.top().first;
         min_heap.pop();
+        //cout << "Checking top source: " << u << endl;
         if (visited[u])
             continue;
         visited[u] = true;
         for (Edge edge : G[u]) {
+            //cout << "Check edge: " << edge << endl;
             int v = edge.dst;
             int weight = edge.weight;
+            //cout << "Visited: " << visited[v] << endl;
+            //cout << "Weight vs Distance: " << distances[u] + weight << " vs " << distances[v] << endl;
             if (!visited[v] && distances[u] + weight < distances[v]) {
+                //cout << "Pushing destination: " << v << endl;
                 distances[v] = distances[u] + weight;
                 previous[v] = u;
                 min_heap.push({v, distances[v]});
             }
         }
+        //cout << "passed all edges" << endl;
     }
+    //cout << "returning" << endl;
     return distances;
 }
 
--- src/dijkstras_main.cpp
+++ src/dijkstras_main.cpp
@@ -1,4 +1,11 @@
 #include "dijkstras.h"
 int main() {
+    Graph G;
+    file_to_graph("src/largest.txt", G);
+    vector<int> previous;
+    vector<int> shortest;
+    previous.resize(G.numVertices, -1);
+    vector<int> dijkstra_path = dijkstra_shortest_path(G, 0, previous);
+    print_path(dijkstra_path, 0);
     return 0;
 }
